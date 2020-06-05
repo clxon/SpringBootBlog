e94a01e6971795eaa88ea79b241da1ba5557d70a
Nicklaus Ning <deeplemon@outlook.com>
2020-06-05 19:37:15 +0800

    Change the IndexController and index.html.

--- src/main/java/me/lemon/demo/web/IndexController.java
+++ src/main/java/me/lemon/demo/web/IndexController.java
@@ -3,14 +3,17 @@ package me.lemon.demo.web;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class IndexController {
     @GetMapping("/")
-    public String index( HttpSession session ){ 
+    public String index(Model m, HttpSession session){
         String name = (String)session.getAttribute("username");
-        System.out.println(name);
+        if (name != null) {
+            m.addAttribute("username",name);
+        }
         return "index";
     }
 }
