0c6e331a04701301d2d5c8aa39bc78582519c683
Nicklaus Ning <deeplemon@outlook.com>
2020-06-05 13:30:52 +0800

    Update.

--- src/main/java/me/lemon/demo/web/ApiController.java
+++ src/main/java/me/lemon/demo/web/ApiController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -17,6 +18,7 @@ public class ApiController {
     @Autowired
     private UserService userService;
 
+    @ResponseBody
     @GetMapping("/jsonQuery")
     public List<User> queryForJson() {
         return userService.queryForUsers();
